f418da4a24fb7df9f059dcc61f848b9803e5c91d
delaraSalimi <140524147+delaraSalimi@users.noreply.github.com>
2023-09-18 15:38:33 +0200

    Update VerifyCsrfToken.php

--- Company/app/Http/Middleware/VerifyCsrfToken.php
+++ Company/app/Http/Middleware/VerifyCsrfToken.php
@@ -12,5 +12,5 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except = [
-      '/developers/action'    ];
+      '/developers/action' , '/companies/action'    ];
 }
